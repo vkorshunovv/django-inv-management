e827b32bee439880a640f68ceca61c5aac5f78e2
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-10-22 21:14:52 +0200

    create layout.html

--- invProject/invApp/views.py
+++ invProject/invApp/views.py
@@ -11,11 +11,11 @@ def home_view(request):
 def product_create_view(request):
     form = ProductForm()
     if request.method == 'POST':
-        form = ProductForm(request.Post)
+        form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('product_list')
-    return redirect(request, 'invApp/product_form.html', {'form': form})
+    return render(request, 'invApp/product_form.html', {'form': form})
     
 #Read View
 def product_list_view(request):
@@ -27,7 +27,7 @@ def product_update_view(request, product_id):
     product = Product.objects.get(product_id=product_id)
     form = ProductForm(instance=product)
     if request.method == 'POST':
-        form = ProductForm(request.Post, instance=product)
+        form = ProductForm(request.POST, instance=product)
         if form.is_valid():
             form.save()
             return redirect('product_list')
